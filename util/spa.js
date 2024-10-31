2f35eefe72d2630b4259aa0ff9a78fdb21c1840a
SahanDhanujaya <sahandhanujaya18@gmail.com>
2024-10-31 23:27:35 +0530

    spa updated to jQuery

--- util/spa.js
+++ util/spa.js
@@ -1,12 +1,21 @@
-$('#dashboard').css.display = 'block';
-$('#customer-form').css.display = 'none';
+$('#dashboard').css('display','block');
+$('#customer-form').css('display','none');
+$('#item-form').css('display','none');
 
-$('#dashboard-nav').on('click',()=>{
-    $('#dashboard').css.display = 'block';
-    $('#customer-form').css.display = 'none';
+$('#dashboard-nav').on('click',function(){
+    $('#dashboard').css('display','block');
+    $('#customer-form').css('display','none');
+    $('#item-form').css('display','none');
 });
 
-$('#customer-nav').on('click',()=>{
-    $('#dashboard').css.display = 'none';
-    $('#customer-form').css.display = 'block';
+$('#customer-nav').on('click',function(){
+    $('#dashboard').css('display','none');
+    $('#customer-form').css('display','block');
+    $('#item-form').css('display','none');
+});
+
+$('#item-nav').on('click',function(){
+    $('#dashboard').css('display','none');
+    $('#customer-form').css('display','none');
+    $('#item-form').css('display','block');
 });
